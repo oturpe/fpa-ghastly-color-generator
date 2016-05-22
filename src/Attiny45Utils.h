d5abd044844981980c1842795fb7c43883933072
Otto Urpelainen <oturpe@iki.fi>
2016-05-22 23:48:03 +0200

    Fix isp programmer & attiny45 library

--- src/Attiny45Utils.h
+++ src/Attiny45Utils.h
@@ -11,10 +11,20 @@ namespace Attiny45 {
 /// Possible prescaler values for timers.
 enum TimerPrescalerValue {
     PSV_1,
+    PSV_2,
+    PSV_4,
     PSV_8,
+    PSV_16,
+    PSV_32,
     PSV_64,
+    PSV_128,
     PSV_256,
-    PSV_1024
+    PSV_512,
+    PSV_1024,
+    PSV_2048,
+    PSV_4096,
+    PSV_8192,
+    PSV_16384
 };
 
 /// Sets timer 0 prescaler to requested value.
@@ -26,5 +36,16 @@ enum TimerPrescalerValue {
 /// \param value
 ///    Requested prescaler value
 void setTimer0Prescaler(TimerPrescalerValue value);
+
+/// Sets timer 1 prescaler to requested value.
+///
+/// This function assumes that Clock Select bits have not been touched yet.
+/// Timer 1 support all listed prescaler values.
+///
+/// \param value
+///    Requested prescaler value
+void setTimer1Prescaler(TimerPrescalerValue value);
+
 }
 
+#endif
